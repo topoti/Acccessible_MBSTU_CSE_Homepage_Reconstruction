ef55b22587c74b7db5e9f7cb1f6b438862f577aa
Debasree97 <atd21821@gmail.com>
2022-07-04 22:16:54 +0600

    topbar visually complete

---
+++ src/Componants/Header/Titlebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titlebar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Titlebar;
--- src/Componants/Header/Topbar.js
+++ src/Componants/Header/Topbar.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import {
   faCircleQuestion,
   faEnvelope,
@@ -12,51 +12,95 @@ import "./Header.css";
 
 const Topbar = () => {
   return (
-    <nav>
-      <Container fluid className="topbar d-flex">
-        <div>
-          <a tabIndex="1" href="mailto:cse@mbstu.ac.bd">
+    <Navbar
+      bg="light"
+      className="d-none d-lg-block px-5"
+      style={{ paddingBottom: "5px", paddingTop: "5px" }}
+    >
+      <Container
+        className="d-flex justify-content-around"
+        style={{ height: "25px", fontSize: "14px" }}
+      >
+        <Nav className="d-flex align-items-center">
+          <Nav.Link
+            className="link"
+            target="_blank"
+            href="mailto:cse@mbstu.ac.bd"
+            tabIndex="1"
+          >
             <FontAwesomeIcon
+              style={{ fontSize: "14px" }}
+              className="me-2"
               aria-label="send email at cse@mbstu.ac.bd"
               icon={faEnvelope}
             />
-            <span>cse@mbstu.ac.bd</span>
-          </a>
-          <a
+            <span className="me-2">cse@mbstu.ac.bd</span>
+          </Nav.Link>
+          <span className="fw-lighter">|</span>
+          <Nav.Link
+            className="link"
             target="_blank"
             href="https://goo.gl/maps/r1ybUXG3DqJDq7Me6"
-            rel="noreferrer"
+            // rel="noreferrer"
             tabIndex="2"
           >
             <FontAwesomeIcon
+              style={{ fontSize: "14px" }}
+              className="me-2"
               aria-label="google map location pin of MBSTU"
               icon={faLocationDot}
             />
-            Santosh, Tangail, Bangladesh
-          </a>
-          <a tabIndex="3" href="https://www.facebook.com/cse.mbstu.11">
+            <span className="me-4">Santosh, Tangail, Bangladesh</span>
+          </Nav.Link>
+          <span className="fw-lighter">|</span>
+          <Nav.Link
+            className="link"
+            target="_blank"
+            tabIndex="3"
+            href="https://www.facebook.com/cse.mbstu.11"
+          >
             <FontAwesomeIcon
+              style={{ fontSize: "14px" }}
+              className="me-2"
               aria-label="facebook page link of CSE,MBSTU"
               icon={faFacebookF}
             />
-          </a>
-        </div>
-
-        <div>
-          <a tabIndex="4" href="https://cse.mbstu.ac.bd/faq">
+          </Nav.Link>
+          <span className="fw-lighter">|</span>
+        </Nav>
+        <Nav className="d-flex align-items-center">
+          <Nav.Link
+            className="link"
+            target="_blank"
+            tabIndex="4"
+            href="https://cse.mbstu.ac.bd/faq"
+          >
             <FontAwesomeIcon
+              style={{ fontSize: "14px" }}
+              className="me-2"
               aria-label="frequently asked question"
               icon={faCircleQuestion}
             />
-            <span>FAQ</span>
-          </a>
-          <a tabIndex="5" href="https://cse.mbstu.ac.bd/login">
-            <FontAwesomeIcon aria-label="login" icon={faLock} />
+            <span className="me-2">FAQ</span>
+          </Nav.Link>
+          <span className="fw-lighter">|</span>
+          <Nav.Link
+            className="link"
+            target="_blank"
+            tabIndex="5"
+            href="https://cse.mbstu.ac.bd/login"
+          >
+            <FontAwesomeIcon
+              style={{ fontSize: "14px" }}
+              className="mx-2"
+              aria-label="login"
+              icon={faLock}
+            />
             <span>Login</span>
-          </a>
-        </div>
+          </Nav.Link>
+        </Nav>
       </Container>
-    </nav>
+    </Navbar>
   );
 };
 
